85f5773886726769f3d8e1d6f228aaa1e379e863
Lambano <78645674+BERSERKYT@users.noreply.github.com>
2024-01-02 16:33:57 +0000

    fixing flag bug

--- web.js
+++ web.js
@@ -3,17 +3,11 @@ function updateCountryCode() {
     var selectedOption = countrySelect.options[countrySelect.selectedIndex];
     var countryCode = selectedOption.getAttribute("data-image");
 
+    countrySelect.style.backgroundImage = "none";
+
     countrySelect.style.backgroundImage = "url('" + countryCode + "')";
 }
+window.onload = updateCountryCode;
 
-// Si vous souhaitez également mettre à jour l'indicatif dans le champ de téléphone
-document.getElementById("country").addEventListener("change", updatePhoneCode);
-
-function updatePhoneCode() {
-    var countrySelect = document.getElementById("country");
-    var selectedOption = countrySelect.options[countrySelect.selectedIndex];
-    var phoneInput = document.getElementById("phone");
-    var phoneCode = selectedOption.getAttribute("data-code");
-
-    phoneInput.placeholder = "Ex. " + phoneCode;
-}
+// Ajouter l'écouteur d'événements "change"
+document.getElementById("country").addEventListener("change", updateCountryCode);
